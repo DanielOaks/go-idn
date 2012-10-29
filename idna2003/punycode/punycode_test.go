e40422b8227a7aaa950116a551f6b01527853576
Hannes Baldursson <hannson@gmail.com>
2012-10-29 00:42:34 +0000

    idna2003/punycode: update copyright to 2012

--- idna2003/punycode/punycode_test.go
+++ idna2003/punycode/punycode_test.go
@@ -1,3 +1,7 @@
+// Copyright 2012 Hannes Baldursson. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 /*
 Test cases extracted from RFC 3492
 
